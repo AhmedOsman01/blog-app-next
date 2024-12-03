9e0f3069258bf4e0cd51163939ce6be23afd8677
Ahmed osman <ahmedosmanelhady@gmail.com>
2024-12-03 16:59:22 +0200

    Add mongodb and save user from Clerk

---
+++ src/lib/actions/user.js
@@ -0,0 +1,45 @@
+import User from "../models/user.model";
+
+import { connect } from "../mongodb/mongoose";
+
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
+) => {
+  try {
+    await connect();
+
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email_addresses[0].email_addresses,
+          username,
+        },
+      },
+      { new: true, upsert: true }
+    );
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to create or update user");
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    await connect();
+    await User.findOneAndDelete({ clerkId: id });
+    return true;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to delete user");
+  }
+};
---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export default async function connectDB() {
+  mongoose.set("strict", true);
+  if (initialized) {
+    console.log("DB connection already connected.");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "blog-app-next",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("DB connection successful.");
+    initialized = true;
+  } catch (error) {
+    console.log("Error connecting to database " + error.message);
+  }
+}
